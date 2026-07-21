880b92ed80d9054d37b11f8a7833daa08a831f06
Srudhi <srudhi24@gmail.com>
2026-07-21 17:39:00 +0530

    Updated Resume

--- script.js
+++ script.js
@@ -75,12 +75,12 @@ $(document).ready(function(){
 // Resume download
 document.getElementById("resume-button-1").onclick = () => {
     window.open(
-      "hhttps://drive.google.com/file/d/1TLeoawBauW-opAeWZKDojbFptT8ZMFmK/view?usp=drive_link"
+      "https://drive.google.com/file/d/1BtCt9xym0vouNOZFcmowymoPyelfowdh/view?usp=drive_link"
     );
   };
   document.getElementById("resume-button-2").onclick = () => {
     window.open(
-      "https://drive.google.com/file/d/1TLeoawBauW-opAeWZKDojbFptT8ZMFmK/view?usp=drive_link"
+      "https://drive.google.com/file/d/1BtCt9xym0vouNOZFcmowymoPyelfowdh/view?usp=drive_link"
     );
   };
 
